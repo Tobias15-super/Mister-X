7d6e5292ace1addd03146d06ca0cd887b711e9f5
Tobias15-super <tobiaslist15@gmail.com>
2025-07-23 19:30:09 +0000

    Integration neuer Code

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,28 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const cloudinary = require("cloudinary").v2
+admin.initializeApp();
 
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
+cloudinary.config({
+  cloud_name: "ddvf141hb",
+  api_key: "627784367818346",
+  api_secret: "luIwzx9FLarg2DAPTbhklCWs3PU"
+});
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+exports.deleteAllPhotos = functions.https.onCall(async (data, context) => {
+  const db = admin.database();
+  const snapshot = await db.ref("locations").once("value");
+  const deletions = [];
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+  snapshot.forEach(child => {
+    const publicId = child.val().publicId;
+    if (publicId) {
+      deletions.push(cloudinary.uploader.destroy(publicId));
+    }
+  });
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+  await Promise.all(deletions);
+  await db.ref("locations").remove();
+  return { success: true };
+});
+
