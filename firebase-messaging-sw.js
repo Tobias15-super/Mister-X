f593eb54d40318b735405d378a03e868af411b57
Tobias15-super <tobiaslist15@gmail.com>
2025-07-23 20:34:59 +0000

    Anpassungen

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,24 +1,8 @@
-importScripts('https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/10.3.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyC-jTMiDjHNTC6cvSKUU44mVbWwT-ToLxQ",
-  authDomain: "mister-x-d6b59.firebaseapp.com",
-  projectId: "mister-x-d6b59",
-  storageBucket: "mister-x-d6b59.firebasestorage.app",
-  messagingSenderId: "616391598963",
-  appId: "1:616391598963:web:da07882b0f481d3000db06"
+  messagingSenderId: "616391598963"
 });
 
 const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage(function(payload) {
-  console.log('[firebase-messaging-sw.js] Hintergrundnachricht:', payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/icon.png'
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
