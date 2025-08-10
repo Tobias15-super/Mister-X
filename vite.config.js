bc2d082c37149a5a87beaacadfec681e9947f2c1
Tobias15-super <tobiaslist15@gmail.com>
2025-08-10 07:32:15 +0000

    sw.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       srcDir: 'src',
       strategies: 'injectManifest',
       injectManifest: {
-        swSrc: 'src/firebase-messaging-sw.js',
+        swSrc: 'src/sw.js',
         swDest: 'sw.js',
       },
       devOptions: {
