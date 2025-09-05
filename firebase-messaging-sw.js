4c79da5b9548be1d044c4b1610d7d6fbc117f9b4
Tobias15-super <tobiaslist15@gmail.com>
2025-09-05 12:24:03 +0000

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -10309,7 +10309,7 @@ Q.prototype.echo = function(n, e) {
   this.sendRequest("echo", { d: n }, e);
 };
 bd();
-Pl([{"revision":null,"url":"assets/index-BDoMbq07.js"},{"revision":null,"url":"assets/index-fJkLp684.css"},{"revision":"09ef28c3e6a78d532004c7e285dc6074","url":"index.html"},{"revision":"d4bd41f8dd12f1517340d931428983fb","url":"registerSW.js"},{"revision":"04fb41277b2d80dfa79441653613f291","url":"favicon.ico"},{"revision":"cf5eaff918a960ce531aa06af4f66583","url":"icons/android-chrome-192x192.png"},{"revision":"b3a2b02ff54274e88cba679738ae3b04","url":"icons/android-chrome-512x512.png"},{"revision":"fe78c2de6cbe40fab54d42c53c641a48","url":"manifest.webmanifest"}] || []);
+Pl([{"revision":null,"url":"assets/index-Dx6Oj05w.js"},{"revision":null,"url":"assets/index-fJkLp684.css"},{"revision":"46788e7df81d0372094a1e2af754e008","url":"index.html"},{"revision":"d4bd41f8dd12f1517340d931428983fb","url":"registerSW.js"},{"revision":"04fb41277b2d80dfa79441653613f291","url":"favicon.ico"},{"revision":"cf5eaff918a960ce531aa06af4f66583","url":"icons/android-chrome-192x192.png"},{"revision":"b3a2b02ff54274e88cba679738ae3b04","url":"icons/android-chrome-512x512.png"},{"revision":"fe78c2de6cbe40fab54d42c53c641a48","url":"manifest.webmanifest"}] || []);
 const zr = {
   apiKey: "AIzaSyC-jTMiDjHNTC6cvSKUU44mVbWwT-ToLxQ",
   authDomain: "mister-x-d6b59.firebaseapp.com",
